e41128d9beda229a673987b135705246b6d1e016
poopparty <obiora.ezenekwe7@gmail.com>
2026-01-02 17:00:52 -0600

    Update AccountSystem.lua

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -1,1 +1,46 @@
+local AccountSystem = {}
 
+AccountSystem.Accounts = {
+    {
+        Username = "aero",
+        Password = "aero123",
+        IsActive = true
+    },
+    {
+        Username = "taz",
+        Password = "taz123",
+        IsActive = true
+    },
+    {
+        Username = "kiwi",
+        Password = "kiwi123",
+        IsActive = true
+    },
+    {
+        Username = "dexter",
+        Password = "dexter123",
+        IsActive = true
+    },
+    {
+        Username = "desire",
+        Password = "desire123",
+        IsActive = true
+    },
+    {
+        Username = "darkstar",
+        Password = "darkstar123",
+        IsActive = true
+    },
+    {
+        Username = "breezy",
+        Password = "breezy123",
+        IsActive = true
+    },
+    {
+        Username = "drdisrespect",
+        Password = "drdisrespect123",
+        IsActive = true
+    }
+}
+
+return AccountSystem
