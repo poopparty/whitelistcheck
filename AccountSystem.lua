6f139531be376e84c272914933fc4d421cbff594
poopparty <obiora.ezenekwe7@gmail.com>
2026-01-02 20:02:47 -0600

    Update AccountSystem.lua

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -4,42 +4,56 @@ AccountSystem.Accounts = {
     {
         Username = "aero",
         Password = "aero123",
-        IsActive = true
+        IsActive = true,
+        HWID = "204d0b5ae4fb36b5b6992b27b12d66f1ff70cfb65f4d53"
     },
     {
         Username = "taz",
         Password = "taz123",
-        IsActive = true
+        IsActive = true,
+        HWID = "taz-hwid-here"
     },
     {
         Username = "kiwi",
         Password = "kiwi123",
-        IsActive = true
+        IsActive = true,
+        HWID = "kiwi-hwid-here"
     },
     {
         Username = "dexter",
         Password = "dexter123",
-        IsActive = true
+        IsActive = true,
+        HWID = "dexter-hwid-here"
     },
     {
         Username = "desire",
         Password = "desire123",
-        IsActive = true
+        IsActive = true,
+        HWID = "41573bb15ad12f343eefa37fdd1703fa8c7eb4765f4d53"
     },
     {
         Username = "darkstar",
         Password = "darkstar123",
-        IsActive = true
+        IsActive = true,
+        HWID = "darkstar-hwid-here"
     },
     {
         Username = "breezy",
         Password = "breezy123",
-        IsActive = true
+        IsActive = true,
+        HWID = "7FEEC11A46B7D2F5ECF5A000E7999222AD098A4062C7C2270B9657AF85494455"
     },
     {
         Username = "drdisrespect",
         Password = "drdisrespect123",
-        IsActive = true
+        IsActive = true,
+        HWID = "drdisrespect-hwid-here"
+    },
+    {
+        Username = "xiaohei",
+        Password = "xiaohei123",
+        IsActive = true,
+        HWID = "xiaohei-hwid-here"
     }
 }
 
